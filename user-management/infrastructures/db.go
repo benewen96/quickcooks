713dcfe821fac05e9308d95119a6ce2eadf57631
Adam Evans <evans.adam.tp@gmail.com>
2023-08-10 18:50:03 +0100

    Split main file for readability

--- user-management/infrastructures/db.go
+++ user-management/infrastructures/db.go
@@ -1,6 +1,8 @@
 package infrastructures
 
 import (
+	"quickcooks/user-management/models"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -10,8 +12,66 @@ func NewGormDB() *gorm.DB {
 	var err error
 	client, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("Error connecting to database")
+	}
+	err = migrateDatabase(client, true)
+	if err != nil {
+		panic("Error migrating database")
+	}
+	return client
+}
+
+func dropAllTables(migrator gorm.Migrator) error {
+	var err error
+	err = migrator.DropTable(&models.Tenant{})
+	if err != nil {
+		return err
+	}
+	err = migrator.DropTable(&models.User{})
+	if err != nil {
+		return err
+	}
+	err = migrator.DropTable(&models.Permission{})
+	if err != nil {
+		return err
+	}
+	err = migrator.DropTable(&models.Role{})
+	if err != nil {
+		return err
+	}
+	err = migrator.DropTable(&models.RoleAssignment{})
+	if err != nil {
+		return err
+	}
+	err = migrator.DropTable(&models.RolePermission{})
+	if err != nil {
+		return err
 	}
 
-	return client
+	return nil
+}
+
+func migrateDatabase(database *gorm.DB, drop bool) error {
+	migrator := database.Migrator()
+
+	if drop {
+		err := dropAllTables(migrator)
+		if err != nil {
+			return err
+		}
+	}
+
+	err := migrator.AutoMigrate(
+		&models.Tenant{},
+		&models.User{},
+		&models.Permission{},
+		&models.Role{},
+		&models.RoleAssignment{},
+		&models.RolePermission{},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- user-management/main.go
+++ user-management/main.go
@@ -2,152 +2,17 @@ package main
 
 import (
 	"fmt"
-	"quickcooks/user-management/infrastructures"
-	"quickcooks/user-management/models"
-	"quickcooks/user-management/repositories"
-	"quickcooks/user-management/services"
-
-	"gorm.io/gorm"
 )
-
-type UserManagementContext struct {
-	RegistrationService  *services.RegistrationService
-	MyProfileService     *services.MyProfileService
-	MyTenantsService     *services.MyTenantsService
-	AuthorizationService *services.AuthorizationService
-}
-
-func newUserManagementContext() (*UserManagementContext, error) {
-	var database = infrastructures.NewGormDB()
-
-	err := migrateDatabase(database, true)
-	if err != nil {
-		return nil, err
-	}
-
-	var userRepository = repositories.NewGormUserRepository(database)
-	var tenantRepository = repositories.NewGormTenantRepository(database)
-	var roleRepository = repositories.NewGormRoleRepository(database)
-	var roleAssignmentRepository = repositories.NewGormRoleAssignmentRepository(database)
-	var rolePermissionRepository = repositories.NewGormRolePermissionRepository(database)
-	var permissionRepository = repositories.NewGormPermissionRepository(database)
-
-	var registrationService = services.NewRegistrationService(userRepository)
-	var myProfileService = services.NewMyProfileService(userRepository)
-	var myTenantsService = services.NewMyTenantsService(tenantRepository, roleRepository, roleAssignmentRepository)
-	var authorizationService = services.NewAuthorizationService(roleRepository, rolePermissionRepository, permissionRepository)
-
-	userManagementContext := &UserManagementContext{
-		RegistrationService:  registrationService,
-		MyProfileService:     myProfileService,
-		MyTenantsService:     myTenantsService,
-		AuthorizationService: authorizationService,
-	}
-
-	err = userManagementContext.seedDatabase()
-	if err != nil {
-		return nil, err
-	}
-
-	return userManagementContext, err
-}
-
-func dropAllTables(migrator gorm.Migrator) error {
-	var err error
-	err = migrator.DropTable(&models.Tenant{})
-	if err != nil {
-		return err
-	}
-	err = migrator.DropTable(&models.User{})
-	if err != nil {
-		return err
-	}
-	err = migrator.DropTable(&models.Permission{})
-	if err != nil {
-		return err
-	}
-	err = migrator.DropTable(&models.Role{})
-	if err != nil {
-		return err
-	}
-	err = migrator.DropTable(&models.RoleAssignment{})
-	if err != nil {
-		return err
-	}
-	err = migrator.DropTable(&models.RolePermission{})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func migrateDatabase(database *gorm.DB, drop bool) error {
-	migrator := database.Migrator()
-
-	if drop {
-		err := dropAllTables(migrator)
-		if err != nil {
-			return err
-		}
-	}
-
-	err := migrator.AutoMigrate(
-		&models.Tenant{},
-		&models.User{},
-		&models.Permission{},
-		&models.Role{},
-		&models.RoleAssignment{},
-		&models.RolePermission{},
-	)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (c *UserManagementContext) seedDatabase() error {
-	err := c.AuthorizationService.SeedAuthorizationData()
-	if err != nil {
-		return err
-	}
-
-	joeBloggs, err := c.RegistrationService.RegisterUser("Joe Bloggs", "joe.bloggs@example.com", "password")
-	if err != nil {
-		return err
-	}
-
-	janeBloggs, err := c.RegistrationService.RegisterUser("Jane Bloggs", "jane.bloggs@example.com", "password")
-	if err != nil {
-		return err
-	}
-
-	tenant, err := c.MyTenantsService.CreateTenantWithAdmin("example_tenant", joeBloggs.ID)
-	if err != nil {
-		return err
-	}
-
-	member, err := c.AuthorizationService.GetRoleByName("member")
-	if err != nil {
-		return err
-	}
-
-	_, err = c.MyTenantsService.AssignTenantRole(tenant.ID, janeBloggs.ID, member.ID)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
 
 func main() {
 	context, err := newUserManagementContext()
 	if err != nil {
 		fmt.Printf("Error creating context: %v", err)
+		return
 	}
 	err = newRouter(context).Run() // listen and serve on 0.0.0.0:8080
 	if err != nil {
 		fmt.Printf("Error starting router: %v", err)
+		return
 	}
 }
