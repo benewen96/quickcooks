bd20fbd543bb96fb0e4d946a812c0ffd4140f9a7
Ben Ewen <benewen14@gmail.com>
2023-08-20 19:01:09 +0100

    add logger to db

--- user-management/infrastructures/db.go
+++ user-management/infrastructures/db.go
@@ -1,15 +1,32 @@
 package infrastructures
 
 import (
+	"log"
+	"os"
 	"quickcooks/user-management/models"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func NewGormDB(connString string) *gorm.DB {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,  // Slow SQL threshold
+			LogLevel:                  logger.Error, // Log level
+			IgnoreRecordNotFoundError: true,         // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,         // Don't include params in the SQL log
+			Colorful:                  false,        // Disable color
+		},
+	)
+
 	var err error
-	client, err := gorm.Open(postgres.Open(connString), &gorm.Config{})
+	client, err := gorm.Open(postgres.Open(connString), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		panic("Error connecting to database")
 	}
