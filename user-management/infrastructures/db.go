967f4de8736c00445ecff5251abd2714ff13cc36
Ben Ewen <benewen14@gmail.com>
2023-08-20 18:08:12 +0100

    remove drop tables comment

--- user-management/infrastructures/db.go
+++ user-management/infrastructures/db.go
@@ -13,41 +13,8 @@ func NewGormDB(connString string) *gorm.DB {
 	if err != nil {
 		panic("Error connecting to database")
 	}
-	if err != nil {
-		panic("Error migrating database")
-	}
 	return client
 }
-
-// func dropAllTables(migrator gorm.Migrator) error {
-// 	var err error
-// 	err = migrator.DropTable(&models.Tenant{})
-// 	if err != nil {
-// 		return err
-// 	}
-// 	err = migrator.DropTable(&models.User{})
-// 	if err != nil {
-// 		return err
-// 	}
-// 	err = migrator.DropTable(&models.Permission{})
-// 	if err != nil {
-// 		return err
-// 	}
-// 	err = migrator.DropTable(&models.Role{})
-// 	if err != nil {
-// 		return err
-// 	}
-// 	err = migrator.DropTable(&models.RoleAssignment{})
-// 	if err != nil {
-// 		return err
-// 	}
-// 	err = migrator.DropTable(&models.RolePermission{})
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
 
 func MigrateDatabase(database *gorm.DB) error {
 	migrator := database.Migrator()
