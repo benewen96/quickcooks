d235b44664beaeb34f1b9d1cd5687c00dfb268a1
Adam Evans <evans.adam.tp@gmail.com>
2023-08-22 16:01:59 +0100

    Moved authorization data seeding into service

--- user-management/services/authorization.go
+++ user-management/services/authorization.go
@@ -18,12 +18,28 @@ func NewAuthorizationService(
 	roleRepository repositories.IRoleRepository,
 	rolePermissionRepository repositories.IRolePermissionRepository,
 	permissionRepository repositories.IPermissionRepository,
-) *AuthorizationService {
-	return &AuthorizationService{
+) (*AuthorizationService, error) {
+	authorizationService := &AuthorizationService{
 		roleRepository:           roleRepository,
 		rolePermissionRepository: rolePermissionRepository,
 		permissionRepository:     permissionRepository,
 	}
+
+	roles, err := authorizationService.seedRoles()
+	if err != nil {
+		return nil, err
+	}
+	permissions, err := authorizationService.seedPermissions()
+	if err != nil {
+		return nil, err
+	}
+
+	err = authorizationService.seedRolePermissions(roles, permissions)
+	if err != nil {
+		return nil, err
+	}
+
+	return authorizationService, nil
 }
 
 // GetRoles returns a list of all application roles and associated permissions
@@ -34,4 +50,76 @@ func (s *AuthorizationService) GetRoles() ([]*models.Role, error) {
 // GetRoleByName attempts to find a role with the given name if it exists
 func (s *AuthorizationService) GetRoleByName(name string) (*models.Role, error) {
 	return s.roleRepository.GetByName(name)
+}
+
+func (s *AuthorizationService) seedRoles() ([]*models.Role, error) {
+	admin, err := s.roleRepository.FindOrCreate(&models.Role{Name: "admin"})
+	if err != nil {
+		return nil, err
+	}
+
+	member, err := s.roleRepository.FindOrCreate(&models.Role{Name: "member"})
+	if err != nil {
+		return nil, err
+	}
+
+	return []*models.Role{admin, member}, nil
+}
+
+func (s *AuthorizationService) seedPermissions() ([]*models.Permission, error) {
+	var permissions []*models.Permission
+
+	resources := []string{
+		"recipe",
+		"plan",
+		"order",
+		"food",
+		"staple",
+		"tenant",
+	}
+
+	actions := []string{
+		"create",
+		"read",
+		"update",
+		"delete",
+	}
+
+	for _, resource := range resources {
+		for _, action := range actions {
+			permission := &models.Permission{Resource: resource, Action: action}
+			permission, err := s.permissionRepository.FindOrCreate(permission)
+
+			if err != nil {
+				return nil, err
+			}
+
+			permissions = append(permissions, permission)
+		}
+	}
+
+	return permissions, nil
+}
+func (s *AuthorizationService) seedRolePermissions(roles []*models.Role, permissions []*models.Permission) error {
+	for _, p := range permissions {
+		_, err := s.rolePermissionRepository.
+			FindOrCreate(&models.RolePermission{
+				RoleID:       roles[0].ID,
+				PermissionID: p.ID,
+			})
+		if err != nil {
+			return err
+		}
+
+		if p.Action == "read" {
+			_, err := s.rolePermissionRepository.FindOrCreate(&models.RolePermission{
+				RoleID:       roles[1].ID,
+				PermissionID: p.ID,
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
